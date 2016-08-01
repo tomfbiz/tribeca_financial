c7b13247c44397aabe34d9ec68bfbc8c714a7c70
Tom Freudenheim <tom.freudenheim@me.com>
2016-08-01 08:53:39 +0100

    update logos and teaser text

--- js/main.js
+++ js/main.js
@@ -1,30 +1,39 @@
- var more_list = {
-	init: function() {
-		$(".more-content").hide();
-		$('.more-link').click(function(e) {
-			e.preventDefault();
-			$(this).hide();
-			$(this).closest(".row").find(".more-content").slideDown();
-			return false;
-		});	
+//  var more_list = {
+// 	init: function() {
+// 		$(".more-content").hide();
+// 		$('.more-link').click(function(e) {
+// 			e.preventDefault();
+// 			$(this).hide();
+// 			$(this).closest(".row").find(".more-content").slideDown();
+// 			return false;
+// 		});	
+// 	}
+// }
+//  var overlay = {
+// 	init: function() {
+// 		$('.play-lists a h5').mouseenter(function(e) {
+// 			$(this).closest(".play-lists").find(".featured-detail").addClass("featured-detail-hover");
+// 		});	
+// 		$('.play-lists a h5').mouseout(function(e) {
+// 			$(this).closest(".play-lists").find(".featured-detail").removeClass("featured-detail-hover");
+// 		});	
+// 	}	
+// }
+	var topnav_toggle = {
+		init: function() {
+			$("div.navbar-header button.navbar-toggle").click(function(){
+				$(this).toggleClass('expanded');
+				$('div.navbar-header').toggleClass('expanded');
+			});
+		}
 	}
-}
- var overlay = {
-	init: function() {
-		$('.play-lists a h5').mouseenter(function(e) {
-			$(this).closest(".play-lists").find(".featured-detail").addClass("featured-detail-hover");
-		});	
-		$('.play-lists a h5').mouseout(function(e) {
-			$(this).closest(".play-lists").find(".featured-detail").removeClass("featured-detail-hover");
-		});	
-	}	
-}
 
 
-$(document).ready(function() {
-	more_list.init();
-	overlay.init();
-	});
+ $(document).ready(function() {
+ 	topnav_toggle.init();
+// 	more_list.init();
+// 	overlay.init();
+ 	});
 
 
-	
+// 	
